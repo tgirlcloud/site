8d6cedca403062c5a39816d1eca41b7474be8349
isabel <isabel@isabelroses.com>
2025-02-19 17:08:07 +0000

    build(nix): actually build

--- default.nix
+++ default.nix
@@ -4,21 +4,46 @@
   nodejs-slim,
   stdenvNoCC,
 }:
-stdenvNoCC.mkDerivation {
+let
+  packageJson = lib.importJSON ./package.json;
+in
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "tgirlcloud-site";
-  version = "0.1.0";
+  inherit (packageJson) version;
 
   src = ./.;
 
   nativeBuildInputs = [
-    pnpm
+    pnpm.configHook
     nodejs-slim
   ];
+
+  pnpmDeps = pnpm.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    hash = "sha256-zntHo+O8Hck3YjkPwGaHhuYvGIqGEj7JP8YWwUL4TPk=";
+  };
+
+  buildPhase = ''
+    runHook preBuild
+
+    pnpm run build
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    cp -r dist/* $out
+
+    runHook postInstall
+  '';
 
   meta = {
     description = "The source code tgirl.cloud site";
     homepage = "https://github.com/tgirlcloud/site";
-    license = lib.licenses.mit;
+    license = lib.licenses.eupl12;
     maintainers = with lib.maintainers; [ isabelroses ];
   };
-}
+})
