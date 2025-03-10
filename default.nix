1fcab99630deacde1420bfaf0f6c56e157d0acc4
isabel <isabel@isabelroses.com>
2025-03-10 10:50:58 +0000

    chore: bump deps

--- default.nix
+++ default.nix
@@ -20,7 +20,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-zntHo+O8Hck3YjkPwGaHhuYvGIqGEj7JP8YWwUL4TPk=";
+    hash = "sha256-Cx0aB6vYjv6hKvkdS+LjY3VuvCqJd9dY5BcnAqEGxTc=";
   };
 
   buildPhase = ''
