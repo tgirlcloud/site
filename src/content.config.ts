5f5ba05be1da3c47b5986eec6f1a4e68dc97c2b6
isabel <isabel@isabelroses.com>
2025-10-13 20:39:45 +0100

    feat: blog

---
+++ src/content.config.ts
@@ -0,0 +1,57 @@
+import { glob, type Loader } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+import { readingTime } from "reading-time-estimator";
+
+const blog = z.object({
+  title: z.string(),
+  description: z.string(),
+  // Transform string to Date object
+  date: z.coerce.date(),
+  updated: z.coerce.date().optional(),
+  image: z.string().optional(),
+  tags: z.array(z.string()),
+  readTime: z.string().optional(),
+  draft: z.boolean().default(false),
+  archived: z.boolean().default(false),
+});
+
+type BlogPost = z.infer<typeof blog>;
+
+const customLoader: Loader = {
+  ...glob,
+  name: "customLoader",
+  load: async (loaderParams) => {
+    const { store } = loaderParams;
+
+    const baseLoader = glob({
+      base: "./src/content",
+      pattern: "**/*.mdx",
+    });
+    await baseLoader.load.call(this, loaderParams);
+
+    let items = [...store.values()];
+    store.clear();
+
+    const sorted = items.sort(
+      (a, b) =>
+        new Date((b.data as BlogPost).date).getTime() -
+        new Date((a.data as BlogPost).date).getTime(),
+    );
+
+    items.forEach((item) => {
+      const readTime = readingTime(item.body ?? "", 200);
+      item.data.readTime = readTime.minutes;
+    });
+
+    sorted.forEach((item) => {
+      store.set({ ...item });
+    });
+  },
+};
+
+const blogCollection = defineCollection({
+  loader: customLoader,
+  schema: blog,
+});
+
+export const collections = { blog: blogCollection };
